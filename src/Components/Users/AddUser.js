11f561add0f1dde65b2717dd0c59cf38c719d67d
karthik-140 <shanigaramkarthik@gmail.com>
2023-02-03 11:41:01 +0530

    error-handling-incase-form-input-empty

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -3,18 +3,28 @@ import React, { useState } from 'react';
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUserName] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error,setError]= useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: "Invalid Input",
+                message: "Please enter valid name and age. [Input should not be empty]"
+            })
             return;
         }
         if (+enteredAge < 1) {
+            setError({
+                title: "Invalid Age",
+                message: "Please enter valid age."
+            })
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -29,9 +39,12 @@ const AddUser = (props) => {
     const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value);
     }
-
-
+    const errorHandler = () =>{
+        setError(null);
+    }
     return (
+        <div>
+        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>User Name</label>
@@ -41,6 +54,7 @@ const AddUser = (props) => {
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
+        </div>
     )
 }
 
